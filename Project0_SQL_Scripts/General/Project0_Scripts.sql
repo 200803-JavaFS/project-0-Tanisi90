567ef1137329deb6a75310c48e4d4423a58c612b
Tanisi90 <furalpanda@yahoo.com>
2020-08-19 08:48:10 -0700

    Correct SQL Script

--- Project0_SQL_Scripts/General/Project0_Scripts.sql
+++ Project0_SQL_Scripts/General/Project0_Scripts.sql
@@ -1,51 +1,63 @@
---Scalar Function
-SELECT LOWER(last_name) AS lower_lname FROM avengers;
 
---Aggregate Function
-SELECT SUM(power_level) AS total_sum FROM avengers WHERE superhero_id < 5;
+CREATE TABLE account (
+	account_id SERIAL PRIMARY KEY,
+	account_balance FLOAT,
+	account_type VARCHAR(8),
+	status BOOLEAN DEFAULT FALSE
+);
 
---subquery
-SELECT COUNT(upper_name) FROM 
-(SELECT UPPER(first_name) AS upper_name FROM avengers) AS upcount 
-WHERE upper_name LIKE '%C%'; 
-
-
---functions
--- or replace is used here in the same that "if exists" is used with drop. It will replace the function if it already exists. 
-CREATE OR REPLACE FUNCTION get_current_time() RETURNS TIME WITH TIME ZONE
-AS $$
--- curent_time is a built value that is just the current time.  
-SELECT current_time;
-$$ LANGUAGE SQL; 
-
-SELECT get_current_time(); 
-
-
-CREATE OR REPLACE FUNCTION avenger_name() RETURNS TABLE(
+CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY,
 	first_name VARCHAR(30),
-	last_name VARCHAR(30)
-) AS
-$func$
+	last_name VARCHAR(30),
+	address VARCHAR(30),
+	city VARCHAR(30),
+	state VARCHAR(2),
+	zip VARCHAR(5),
+	social_number VARCHAR(11),
+	username VARCHAR(30),
+	password TEXT,
+	email VARCHAR(50)
+);
 
-SELECT first_name, last_name FROM avengers;
 
-$func$ LANGUAGE SQL;
-	
+CREATE TABLE user_accounts(
+	user_id_fk SERIAL REFERENCES users(user_id),
+	account_id_fk SERIAL REFERENCES account(account_id),
+	id SERIAL PRIMARY KEY
+);
 
-SELECT * FROM avenger_name();
+DROP TABLE account CASCADE;
 
-CREATE OR REPLACE FUNCTION avenger_power_under(lpower INTEGER) RETURNS SETOF avengers
-AS
-$$
-	SELECT * FROM avengers WHERE power_level < lpower; 
-$$ LANGUAGE SQL;
 
-SELECT * FROM avenger_power_under(1000);  
+DROP TABLE users CASCADE;
+DROP TABLE user_accounts CASCADE;
+
+
+--ALTER TABLE public.user_accounts ADD CONSTRAINT user_accounts_account_id_fk_fkey FOREIGN KEY (account_id_fk) REFERENCES account(account_id);
+
+SELECT * FROM Users WHERE username = username AND password = password;
+
+INSERT INTO users (first_name , last_name , address , city , state , zip , username, PASSWORD, email )
+VALUES ('Brittany', 'Epstein', '234 S I Hope This Works', 'Chandler', 'AZ', '76575', 'Tanisi,90', 'booyah1', 'Britt90609@aol.com');
+
+DELETE FROM users WHERE username = 'Tanisi90' AND PASSWORD = 'booyah1'; 
+
+DELETE FROM account WHERE account_id = 1;
+
+TRUNCATE account CASCADE;
+TRUNCATE users CASCADE;
+TRUNCATE user_accounts CASCADE;
+
+SELECT * FROM Users WHERE username = 'Tanisi';
+
+SELECT * FROM account JOIN user_accounts  ON account_id = user_accounts.account_id_fk 
+WHERE user_id_fk = 19 AND status = TRUE;
+
+
 
 CREATE OR REPLACE FUNCTION trigger_set_time() RETURNS TRIGGER 
 AS $$
---Returning a Trigger allows for use of some special varaibles NEW and OLD which represent the 
---state of the database before and after the event. 
 
 BEGIN
 	NEW.update_at = NOW();
@@ -53,14 +65,16 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql; 
 
+ALTER TABLE account ADD COLUMN update_at TIMESTAMP;
 
-ALTER TABLE avengers ADD COLUMN update_at TIMESTAMP;
-
-CREATE TRIGGER set_time BEFORE UPDATE ON avengers FOR EACH ROW
+CREATE TRIGGER set_time BEFORE UPDATE ON account FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_time();
 
-UPDATE avengers SET power_level = 3000 WHERE superhero_id = 5; 
+SELECT * FROM Account WHERE status = FALSE;
 
+SELECT account_balance FROM account, users WHERE account_id = 19;
 
+UPDATE account SET status = TRUE WHERE account_id = 19;
 
-
+UPDATE account SET account_type = 'Admin' WHERE account_id = '19';
+UPDATE account SET account_type = 'Admin' WHERE account_id = '18';
